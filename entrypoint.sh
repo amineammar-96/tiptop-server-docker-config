90d3e783513d26894eadf9dad094bbcb4fe0935a
Ammar amine <83842072+amineammar-96@users.noreply.github.com>
2024-04-08 01:03:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-echo 'hello php'
 
 chown -R www-data:www-data /var/www/html
 PHP_MEMORY_LIMIT=500M
-php bin/console make:migration
-php bin/console doctrine:migrations:migrate
-php bin/console doctrine:fixtures:load
-php bin/console cache:clear --env=prod
+composer install
+php bin/console d:d:d --force --if-exists -n
+php bin/console d:d:c -n
+php bin/console d:m:m -n
+php bin/console d:f:l -n
+php bin/console cache:clear --no-warmup
+ 
+ 
 # Add any other commands here
 php bin/console app:reset-game
+chown -R www-data:www-data /var/www/html/
 exec apache2-foreground
