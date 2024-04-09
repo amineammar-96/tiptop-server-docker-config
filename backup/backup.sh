95fdfc79958f5c48db26537254db471d6a1b0e03
Ammar amine <83842072+amineammar-96@users.noreply.github.com>
2024-04-10 00:52:40 +0200

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -33,18 +33,9 @@ backup_mysql_databases() {
     echo "Backup for all MySQL databases in container $container_name completed."
 }
 
-delete_old_backups() {
-    local backup_directory="$1"
-    local weeks_to_keep="$2"
 
-    echo "Deleting backups older than $weeks_to_keep weeks in $backup_directory"
 
-    find "$backup_directory" -type f -name "*.sql" -mtime +$((7 * $weeks_to_keep)) -delete
-
-    echo "Old backups deletion completed."
-}
-
-backup_docker_container() {
+backup_container() {
     local container_name="$1"
     local backup_directory="$2"
 
@@ -59,24 +50,46 @@ backup_docker_container() {
 
 
 
+delete_old_backups() {
+    local backup_directory="$1"
+    local weeks_to_keep="$2"
+
+    echo "Deleting backups older than $weeks_to_keep weeks in $backup_directory"
+
+    find "$backup_directory" -type f -name "*.sql" -mtime +$((7 * $weeks_to_keep)) -delete
+
+    echo "Old backups deletion completed."
+}
+
+
+#delete old backups 
+delete_old_backups "/backup" 2
+
 
 # docker folder backup
 backup_docker_folder "/backup/docker" "docker"
-
-#delete old backups 
-delete_old_backups "/backup/docker" 2
-
 
 
 # databases backups
 backup_mysql_databases "docker-db-1" "/backup/databases/prod/"
 backup_mysql_databases "docker-db_test-1" "/backup/databases/test/"
 
-#delete old backups 
-delete_old_backups "/backup/databases/prod" 2
-delete_old_backups "/backup/databases/test" 2
 
 
-
-
+# Backup Docker containers
+backup_container "docker-frontend-1" "/backup/containers"
+backup_container "docker-frontend-dev-1" "/backup/containers"
+backup_container "docker-backend-dev-1" "/backup/containers"
+backup_container "docker-backend-1" "/backup/containers"
+backup_container "docker-backend-preprod-1" "/backup/containers"
+backup_container "docker-frontend-staging-1" "/backup/containers"
+backup_container "docker-backend-staging-1" "/backup/containers"
+backup_container "docker-frontend-preprod-1" "/backup/containers"
+backup_container "docker-gateway-1" "/backup/containers"
+backup_container "docker-sonarqube-1" "/backup/containers"
+backup_container "docker-sonarqube-db-1" "/backup/containers"
+backup_container "docker-prometheus-1" "/backup/containers"
+backup_container "docker-grafana-1" "/backup/containers"
+backup_container "docker-portainer-1" "/backup/containers"
+backup_container "docker-alertmanager-1" "/backup/containers"
 
